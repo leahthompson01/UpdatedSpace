f40ad0d499a5c537f231e4fc9687c7e9c3a7ac2f
catmcclelland <84889884+catmcclelland@users.noreply.github.com>
2022-02-13 08:35:16 -0500

    rocket launch api carousel functionality

--- launches.js
+++ launches.js
@@ -8,10 +8,24 @@ async function sendApiRequest(){
     console.log(response);
     let data = await response.json();
     console.log(data);
-}
-    /*for (let i=1; i<6; i++){
-
-        useApiData(data);
+    for (let i=0; i<5; i++){
+        let num = i;
+        useApiData(data, num);
         
+}}
+function useApiData(data,num){
+    document.querySelector(`#launch-title-${num}`).innerHTML = data.result[num].name;
+    document.querySelector(`#launch-date-${num}`).innerHTML = data.result[num].date_str;
+    document.querySelector(`#launch-info-${num}`).innerHTML= data.result[num].launch_description;
+    let spaceCenter=data.result[num].pad.location.name;
+    let countryOf=data.result[num].pad.location.country;
+    document.querySelector(`#launch-pad-${num}`).innerHTML= `${spaceCenter}, ${countryOf}`;
+    if (data.result[num].weather_summary == null){
+        document.querySelector(`#launch-weather-summary-${num}`).innerHTML = "";
+    }
+    else{
+        let weather = data.result[num].weather_summary
+    document.querySelector(`#launch-weather-summary-${num}`).innerHTML = `Weather: ${weather}`;
 }
-*/
+    
+};
